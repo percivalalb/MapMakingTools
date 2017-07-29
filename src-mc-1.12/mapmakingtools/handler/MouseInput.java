bf7d1c129d77b611eb1b10fc27f22078c23fde5a
ProPercivalalb <propercivalalb@gmail.com>
2017-07-29 19:13:12 +0100

    Use proper key event

--- src-mc-1.12/mapmakingtools/handler/MouseInput.java
+++ src-mc-1.12/mapmakingtools/handler/MouseInput.java
@@ -1,19 +1,43 @@
 package mapmakingtools.handler;
 
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+
+import mapmakingtools.MapMakingTools;
+import mapmakingtools.helper.ReflectionHelper;
+import mapmakingtools.network.PacketDispatcher;
+import mapmakingtools.network.packet.PacketOpenItemEditor;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-//TODO
 public class MouseInput {
+
+    public static final KeyBinding KEY_ITEM_EDITOR = new KeyBinding("mapmakingtools.key.itemeditor", Keyboard.KEY_M, "mapmakingtools.key.category");
 
 
 	@SubscribeEvent
 	public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardInputEvent.Pre event) {
-		
-	}
-
-	@SubscribeEvent
-	public void onGuiKeyboardEvent(GuiScreenEvent.KeyboardInputEvent.Post event) {
-		
+		GuiScreen guiScreen = event.getGui();
+		if(guiScreen instanceof GuiContainer && KEY_ITEM_EDITOR.isActiveAndMatches(Keyboard.getEventKey())) {
+			GuiContainer guiContainer = (GuiContainer)guiScreen;
+			int x = Mouse.getEventX() * guiScreen.width / guiScreen.mc.displayWidth;
+			int y = guiScreen.height - Mouse.getEventY() * guiScreen.height / guiScreen.mc.displayHeight - 1;
+			
+			if(Keyboard.getEventKeyState()) {
+				Slot slot = guiContainer.getSlotUnderMouse();
+                    
+				if(slot != null && slot.inventory instanceof InventoryPlayer && slot.isEnabled()) {
+					if(slot.getHasStack()) {
+						PacketDispatcher.sendToServer(new PacketOpenItemEditor(slot.getSlotIndex()));
+						event.setCanceled(true);
+					}
+        		}
+			}
+		}
 	}
 }
