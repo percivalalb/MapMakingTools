f05c4607d021342ddfacebdf6df2edc79b5ff44e
ProPercivalalb <propercivalalb@gmail.com>
2018-09-22 11:42:06 +0100

    Require forge 14.23.4.2703 or above

--- src-mc-1.12.2/mapmakingtools/lib/Reference.java
+++ src-mc-1.12.2/mapmakingtools/lib/Reference.java
@@ -9,7 +9,7 @@ public class Reference {
 	public static final String 		 MOD_ID 	     = "mapmakingtools";
 	public static final String		 MOD_NAME        = "Map Making Tools";
 	public static final String 		 MOD_VERSION     = "${MOD_VERSION}";
-	public static final String 		 MOD_DEPENDENCIES = "required-after:Forge@[9.10.1.857,)";
+	public static final String 		 MOD_DEPENDENCIES = "required-after:Forge@[14.23.4.2703,)";
 	public static final String 		 SP_CLIENT 		 = "mapmakingtools.proxy.ClientProxy";
 	public static final String 		 SP_SERVER		 = "mapmakingtools.proxy.CommonProxy";
 	public static final String       CHANNEL_NAME    = "MMT";
